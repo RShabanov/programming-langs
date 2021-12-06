62b61e08ec6872c6eadb7d21e98e57dd6901b48d
Roman Shabanov <shabanov.roman0@gmail.com>
2021-12-06 18:52:04 +0800

    Remove dir 'interpreter_rust'

--- interpreter_rust/interpreter/src/node.rs
+++
@@ -1,8 +0,0 @@
-use super::token::Token;
-
-#[derive(Debug)]
-pub(crate) enum Node {
-    Number(Token),
-    BinOp(Box<Node>, Token, Box<Node>),
-    UnaryOp(Token, Box<Node>),
-}
--- interpreter_rust/interpreter/src/token/mod.rs
+++
@@ -1,7 +0,0 @@
-mod token_struct;
-mod token_type;
-
-pub use self::{
-    token_struct::Token,
-    token_type::TokenType,
-};
